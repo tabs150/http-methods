68e6364772fa0a65b8b61cb683c5db039ec809f8
Thabiso Pheko <tabs150@gmail.com>
2019-04-19 16:01:18 +0200

    first commit

---
+++ js/app.js
@@ -0,0 +1,7 @@
+const httpForm = document.getElementById("httpForm");
+const itemInput = document.getElementById("itemInput");
+const imageInput = document.getElementById("imageInput");
+const submitBtn = document.getElementById("submitBtn");
+const itemList = document.querySelector(".items");
+const feedback = document.querySelector(".feedback");
+let editedItemID = 0;
