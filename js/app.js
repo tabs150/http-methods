9a983899d9a96eb407fc88f0c6ae17a2d2989622
Thabiso Pheko <tabs150@gmail.com>
2019-05-03 15:21:13 +0200

    added showItems function

--- js/app.js
+++ js/app.js
@@ -6,16 +6,16 @@ const itemList = document.querySelector(".items");
 const feedback = document.querySelector(".feedback");
 let editedItemID = 0;
 
-const url = 'http://5ccb0b7454c8540014835258.mockapi.io/items';
+const url = 'https://5ccb0b7454c8540014835258.mockapi.io/items';
 
 // load items
-const func = document.addEventListener("DomContentLoaded", function() {
-
+const func = document.addEventListener("DOMContentLoaded", function() {
+  getItemsAPI(showItems);
 });
 
 // get items
 function getItemsAPI(callBack) {
-  const url = 'http://5ccb0b7454c8540014835258.mockapi.io/items';
+  const url = 'https://5ccb0b7454c8540014835258.mockapi.io/items';
 
   const ajax = new XMLHttpRequest();
 
@@ -34,4 +34,30 @@ function getItemsAPI(callBack) {
   };
 
   ajax.send();
+}
+
+// show items
+function showItems(data) {
+  const items = JSON.parse(data); 
+  // console.log(items);
+  let info = '';
+
+  items.forEach(item => {
+    info += `
+    <li class="list-group-item d-flex align-items-center justify-content-between flex-wrap item my-2">
+      <img src="${item.avatar}" alt="" id="itemImage" class="itemImage img-thumbnail">
+      <h6 id="itemName" class="itemName">${item.name}</h6>
+      <div class="icons">
+        <a href="#" class="itemIcon mx-2 edit-icon" data-id="${item.id}">
+          <i class="fas fa-edit"></i>
+        </a>
+        <a href="#" class="itemIcon mx-2 delete-icon" data-id="${item.id}">
+          <i class="fas fa-trash"></i>
+        </a>
+      </div>
+    </li>
+    `
+  });
+
+  itemList.innerHTML = info;
 }
