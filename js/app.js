d75bb1b7ed6a6a3d85029ae480937de056c79d79
Thabiso Pheko <tabs150@gmail.com>
2019-05-03 13:43:10 +0200

    added getItemsAPI function

--- js/app.js
+++ js/app.js
@@ -6,4 +6,32 @@ const itemList = document.querySelector(".items");
 const feedback = document.querySelector(".feedback");
 let editedItemID = 0;
 
-let url;
+const url = 'http://5ccb0b7454c8540014835258.mockapi.io/items';
+
+// load items
+const func = document.addEventListener("DomContentLoaded", function() {
+
+});
+
+// get items
+function getItemsAPI(callBack) {
+  const url = 'http://5ccb0b7454c8540014835258.mockapi.io/items';
+
+  const ajax = new XMLHttpRequest();
+
+  ajax.open('GET', url, true);
+  
+  ajax.onload = function(){
+    if(this.status === 200) {
+      callBack(this.responseText);
+    } else {
+      console.log('Something went wrong!');
+    }
+  };
+
+  ajax.onerror = function() {
+    console.log('There was an error!');    
+  };
+
+  ajax.send();
+}
